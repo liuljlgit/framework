2ea3c18a6539725a1671229b41904f58ccd429f9
刘立俊 <645604899@qq.com>
2020-07-23 00:05:14 +0800

    树构建

--- frame-auth/src/main/java/com/cloud/frame/frameauth/util/TreeBuilder.java
+++ frame-auth/src/main/java/com/cloud/frame/frameauth/util/TreeBuilder.java
@@ -20,6 +20,10 @@ import java.util.stream.Collectors;
 /**
  * @author Liulj
  * @version v 0.1 2019/9/24 14:50
+ * 使用方式：
+ *  1.定义一个类M继承自Node节点类
+ *  2.调用buildTree方法返回List<Node>
+ *  3.将返回的List<Node>强转成自定义的类M
  */
 @Slf4j
 @NoArgsConstructor
@@ -30,12 +34,12 @@ public class TreeBuilder {
      * 构建树形结构
      * @return
      */
-    public List<Node> buildTree(List<Node> allNodes,String rootPid) {
+    public static List<Node> buildTree(List<Node> allNodes,String rootPid) {
         List<Node> treeNodes = Lists.newArrayList();
         List<Node> rootNodes = getRootNodes(allNodes,rootPid);
         Map<String, List<Node>> pidMap = allNodes.stream()
-                .filter(e->StringUtils.isNotEmpty(e.getPid()))
-                .collect(Collectors.groupingBy(Node::getPid));
+                .filter(e->StringUtils.isNotEmpty(e.getPId()))
+                .collect(Collectors.groupingBy(Node::getPId));
         for (Node rootNode : rootNodes) {
             buildChildNodes(rootNode,pidMap);
             treeNodes.add(rootNode);
@@ -49,10 +53,10 @@ public class TreeBuilder {
      * @param rootPid
      * @param allNodes
      */
-    private List<Node> getRootNodes(List<Node> allNodes,String rootPid) {
+    private static List<Node> getRootNodes(List<Node> allNodes,String rootPid) {
         List<Node> rootNodes = Lists.newArrayList();
         for (Node n : allNodes){
-            if (rootPid.equals(n.getPid())) {
+            if (rootPid.equals(n.getPId())) {
                 rootNodes.add(n);
             }
         }
@@ -66,7 +70,7 @@ public class TreeBuilder {
      * @param pNode
      * @param pidMap
      */
-    private void buildChildNodes(Node pNode, Map<String, List<Node>> pidMap) {
+    private static void buildChildNodes(Node pNode, Map<String, List<Node>> pidMap) {
         List<Node> childNodes = pidMap.getOrDefault(pNode.getId(), Lists.newArrayList()).stream()
                 .sorted(Comparator.comparing(Node::getWgt))
                 .collect(Collectors.toList());
@@ -78,6 +82,7 @@ public class TreeBuilder {
         }
     }
 
+
     @Data
     @NoArgsConstructor
     @AllArgsConstructor
@@ -88,10 +93,10 @@ public class TreeBuilder {
         private String id;
 
         @ApiModelProperty(value = "父级主键",hidden = true)
-        private String pid;
+        private String pId;
 
         @ApiModelProperty(value = "父级名称",hidden = true)
-        private String pname;
+        private String pName;
 
         @ApiModelProperty(value = "权重",hidden = true)
         private Integer wgt = 1;
